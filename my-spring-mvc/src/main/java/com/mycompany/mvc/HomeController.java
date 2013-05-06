565810f9c64a6d36dee48f411f2ee65eab9c5c63
Juntae <workjjt@gmail.com>
2013-05-07 03:08:13 +0900

    정렬, 리스트 갯수 변경 기능 추가
    Signed-off-by: Juntae <workjjt@gmail.com>

--- my-spring-mvc/src/main/java/com/mycompany/mvc/HomeController.java
+++ my-spring-mvc/src/main/java/com/mycompany/mvc/HomeController.java
@@ -51,76 +51,94 @@ public class HomeController {
 		int listSize = 10; //리스트 사이즈 설정
 		int pageSize = 5; ///페이지 당 보여줄 번호 수[1],[2],[3],[4],[5]
 		
+		int currentPage = 1; //페이지 번호
 		int totalCount = 0; //총 레코드 갯수
 		int pageCount = 0; //총 페이지 수
-		int currentPage = 1; //페이지 번호
-		int currentPageList = 0;	//페이지 순서
-		int nextPageList = 0;
-		int prevPageList = 0;
-		int pageStartNo = 0;
-		int pageEndNo = 0;
 		
-		int startRow = 0;
-		int endRow = 11;
+		int pageGroupCount = 0;	//총 페이지 그룹 수
+		int currentPageGroup = 0;	//현재 페이지 그룹 번호(1그룹{[1],[2]}, 2그룹{[3],[4]}, 3그룹{[5],[6]})
+		int nextPageList = 0;	//[다음]에 대한 페이지 번호
+		int prevPageList = 0;	// [이전]에 대한 페이지 번호
+		int pageStartNo = 0;	//페이지 시작 번호
+		int pageEndNo = 0;		//페이지 끝 번호
+		
+		int startRow = 0;		//시작 데이터 번호
+		int endRow = 0;		//끝 데이터 번호
+		
 		String sidx = "employee_id";		//정렬 대상
 		String sord = "asc";				//정렬 방법
 		
-		//페이징 처리
+		List<Employees> list;		//데이터 저장 리스트
+		System.out.println(request.getParameter("sidx"));
+		if (request.getParameter("sidx") != null) {
+			sidx = request.getParameter("sidx");
+		}
+		if (request.getParameter("list_size") != null) {
+			listSize = Integer.parseInt(request.getParameter("list_size"));
+		}
 		if (request.getParameter("page") != null) {
 			currentPage = Integer.parseInt(request.getParameter("page"));
 		}
-
-		totalCount = employeesService.getTotalCount();	//전체 테이블 갯수
 		
-		pageCount = totalCount / listSize + (totalCount % listSize>0 ? 1 : 0);
+		//--------------페이징 처리 부분----------------------------------------
+		//총 레코드(로우) 수
+		totalCount = employeesService.getTotalCount();
 		
-		if(currentPage > pageCount){
+		//총 페이지 수
+		pageCount = totalCount / listSize + (totalCount % listSize > 0 ? 1 : 0);
+		if(currentPage > pageCount){ //현재 페이지는 총 페이지 수보다 클수 없다.
 			currentPage = pageCount;
 		}
 		
-		startRow  = listSize * currentPage - listSize;
-		endRow = startRow + listSize + 1;
+		//총 페이지 그룹 수
+		pageGroupCount = pageCount / pageSize + (pageCount % pageSize > 0 ? 1 : 0);
 		
-		currentPageList = currentPage/pageSize+( currentPage%pageSize>0 ? 1:0);
-		pageEndNo = currentPageList * pageSize;	
+		//현재 페이지 그룹 번호
+		currentPageGroup = currentPage / pageSize + ( currentPage % pageSize > 0 ? 1 : 0);
+	
+		//페이지 출력 시작, 끝 번호
+		pageEndNo = currentPageGroup * pageSize;	
 		pageStartNo = pageEndNo-(pageSize-1);
 		if (pageEndNo > pageCount) {
 			pageEndNo = pageCount;
 		}
-		
+		//[이전][다음]에 대한 페이지 번호
 		prevPageList = pageStartNo - 1;
 		nextPageList = pageStartNo + pageSize;
 		if(prevPageList < 1){
 			prevPageList=1;
 		}
-		if (nextPageList > pageCount) {
-			// 다음 페이지보다 전체페이지 수보가 클경우
+		if (nextPageList > pageCount) { // 다음 페이지보다 전체페이지 수가 클경우
 			nextPageList = pageCount / pageSize * pageSize + 1;
-			// next_pageno=total_page
-			// 다음 페이지 = 전체페이지수 / 페이지당 보여줄 번호수 * 페이지당 보여줄 번호수 + 1
-			// ex) = 76 / 5 * 5 + 1 ????????
 		}
+		//--------------페이징 처리 부분 끝--------------------------------------
 		
-		System.out.println("totalCount:" + totalCount);
-		System.out.println("pageCount:" + pageCount);
-		System.out.println("currentPage:" + currentPage);
-		System.out.println("currentPageList:" + currentPageList);
-		System.out.println("prevPageList:" + prevPageList);
-		System.out.println("nextPageList:" + nextPageList);
-		System.out.println("pageStartNo:" + pageStartNo);
-		System.out.println("pageEndNo:" + pageEndNo);
+		// 레코드(로우) 시작 번호와 끝 번호 설정
+		startRow  = listSize * currentPage - listSize;
+		endRow = startRow + listSize + 1;
 		
-		List<Employees> list = employeesService.getEmployeeList(startRow, endRow, sidx, sord);
+		// 데이터 가져오기
+		list = employeesService.getEmployeeList(startRow, endRow, sidx, sord);
+		
+		model.addAttribute("listSize", listSize);
+		model.addAttribute("pageSize", pageSize);
+		model.addAttribute("currentPage", currentPage);
+		
+		model.addAttribute("totalCount", totalCount);
+		model.addAttribute("pageCount", pageCount);
+		model.addAttribute("pageGroupCount", pageGroupCount);
 		
 		model.addAttribute("pageStartNo", pageStartNo);
 		model.addAttribute("pageEndNo", pageEndNo);
 		model.addAttribute("prevPageList", prevPageList);
 		model.addAttribute("nextPageList", nextPageList);
-		model.addAttribute("listSize", listSize);
-		model.addAttribute("pageSize", pageSize);
-		model.addAttribute("currentPage", currentPage);
-		model.addAttribute("totalCount", totalCount);
-		model.addAttribute("pageCount", pageCount);
+		model.addAttribute("currentPageGroup", currentPageGroup);
+		
+		model.addAttribute("startRow", startRow);
+		model.addAttribute("endRow", endRow);
+		
+		model.addAttribute("sidx", sidx);
+		
 		model.addAttribute("list", list);
 		
 		return "board/board_list";
@@ -148,5 +166,5 @@ public class HomeController {
 		
 		return root;
 	}
-	
+
 }
